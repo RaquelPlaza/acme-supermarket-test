335189e66f0cf32e51cebb637eec4c3136310350
Raquel Plaza <contact@raquelplaza.com>
2017-11-28 00:30:28 +0000

    Restructure and creation of both offer functions

--- components/acmeCart.js
+++ components/acmeCart.js
@@ -27,57 +27,82 @@ class AcmeCart extends Component {
 			  
 			],
 			buyOneGetOneItemCode: "FR1",
-			otherOfferItems: []
+			bulkDiscountItemCode: "SR1",
 		};
 
-		this.addToCart=this.addToCart.bind(this);
+		this.addToCart= this.addToCart.bind(this);
 		this.buyOneGetOne = this.buyOneGetOne.bind(this);
+		this.bulkDiscount = this.bulkDiscount.bind(this);
 		this.getItemPrice = this.getItemPrice.bind(this);
 
 	}
 
 	addToCart(event, item) {
 		event.preventDefault();
-		// console.log(event.target.value);
+
+		console.log(event);
+
+		const discountedTotal = this.buyOneGetOne('FR1', 5) || this.bulkDiscount('SR1', 4);
+
+		this.setState(prevState => ({
+		  cartContents: [...prevState.cartContents, {
+					id: 'FR1',
+					qty: 5,
+					total: 0,
+					discountedTotal: discountedTotal
+				}]
+		}));
 		
-		// this.setState(prevState => ({
-		//   cartContents: [...prevState.cartContents, item]
-		// }));
 
 	}
 
-	buyOneGetOne() {
+	buyOneGetOne(itemAddedId, qty) {
 
 		let itemsInOffer;
 		
-		if(this.state.cartContents && this.state.buyOneGetOneItemCode) {	
-			itemsInOffer = this.state.cartContents.filter((each) => each.id == this.state.buyOneGetOneItemCode);
+		if(this.state.buyOneGetOneItemCode) {	
+			itemsInOffer = (itemAddedId == this.state.buyOneGetOneItemCode);
 
-		for (var i = 0; i < itemsInOffer.length; i++) {
+			if(itemsInOffer) {
 
-			let chargableItemsQty = 0;
+				let chargableItemsQty = 0;
 
-		if (itemsInOffer[i].qty >= 2) {
-			chargableItemsQty = Math.floor(itemsInOffer[i].qty/2) + (itemsInOffer[i].qty % 2);	
-		}
+				if (qty >= 2) {
+					chargableItemsQty = Math.floor(qty/2) + (qty % 2);	
+					const resolvedPriceAfterOffer = chargableItemsQty * this.getItemPrice(itemAddedId);
 
-		const resolvedPriceAfterOffer = chargableItemsQty * this.getItemPrice(itemsInOffer[i].id);
-		console.log('res', resolvedPriceAfterOffer);
+					return resolvedPriceAfterOffer;
 
-		}
+				} 
 
-		}
-			
+			}
+		}		
 	}
 
-	getItemPrice(itemId) {
-			for (var i = 0; i < this.state.products; i++) {
-			if(this.state.products[i].id == itemId) {
-				console.log(this.state.products[i]);
-				return this.state.products[i].price;
+	bulkDiscount(itemAddedId, qty) {
+		if(this.state.bulkDiscountItemCode) {
+			itemsInOffer = (itemAddedId == this.state.bulkDiscountItemCode);
+		}
+
+		if(itemsInOffer) {
+
+			if (qty >= 3) {
+				reducedPrice = this.getItemPrice(itemAddedId) - 0.5;	
+				const resolvedPriceAfterOffer = chargableItemsQty * reducedPrice;
+
+				return resolvedPriceAfterOffer;
 			}
 		}
 
+	}
+
+	getItemPrice(itemId) {
+		
+		for (var i = 0; i < this.state.products.length; i++) {
+			if(this.state.products[i].id == itemId) {
+				return this.state.products[i].price;
+			}
+		}
 
 	}
 
@@ -88,11 +113,15 @@ class AcmeCart extends Component {
 			cartContents: [
 				{
 					id: 'FR1',
-					qty: 3
+					qty: 3,
+					total: 0,
+					discountedTotal: 0
 				}, 
 				{
 					id: 'SR1',
-					qty: 2
+					qty: 2,
+					total: 0,
+					discountedTotal: 0
 				}]
 			});
 		// this.buyOneGetOne();
@@ -100,23 +129,28 @@ class AcmeCart extends Component {
 	}
 
 	render() {
-		// console.log(this.state.cartContents);
-		// console.log(this.state.buyOneGetOneItem);
-		this.buyOneGetOne();
+		//const discountedPrice = this.buyOneGetOne();
 
-		// const contents = Object.keys(this.state.cartContents).map(function(each, key) {
+		const shoppingBasket = this.state.cartContents;
+		const contents = Object.keys(shoppingBasket).map(function(key) {
 
-		//  return <li key="key">{this.state.cartContents[key].id}</li>
-		//  });
+		let price
+
+		 return <li key={key}>{shoppingBasket[key].id} Quantity: {shoppingBasket[key].qty} £{shoppingBasket[key].discountedTotal}</li>
+		 });
 
 		return (
 	    <div className='acme-cart'>
 	      <h1>Acme cart</h1>
 	      <form>
-	      <label htmlFor="basket">Basket</label>
-			<input name="basket" type="text" />
-			<input name="submit" type="submit" onClick={this.addToCart} />
+	      	<label htmlFor="qty">Fruit tea</label>
+			<input name="qty" type="text" />
+			<input name="submit" type="button" value="Add" onClick={this.addToCart} />
+			<label htmlFor="SR1">Strawberries</label>
+			<input name="SR1" type="text" />
+			<input name="submit" type="button" value="Add" onClick={this.addToCart} />
 	      </form>
+	      <h2>Cart contents</h2>
 	      <div><ul>{contents}</ul></div>
 	    </div>
 	  	);
